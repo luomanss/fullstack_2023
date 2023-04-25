6da7a5e7f6978e7b08922bedc25ab1e4b4e8ce40
Sami Luomansuu <sami.luomansuu@gmail.com>
2023-04-25 19:15:58 +0300

    Added part1 course_info. Exercises 1.1-1.5 done.

---
+++ part1/course_info/src/App.js
@@ -0,0 +1,59 @@
+const Header = ({ course }) => {
+  return <h1>{course.name}</h1>;
+};
+
+const Part = ({ part }) => {
+  return (
+    <p>
+      {part.name} {part.exercises}
+    </p>
+  );
+};
+
+const Content = ({ parts }) => {
+  return (
+    <>
+      {parts.map((part) => (
+        <Part key={part.name} part={part} />
+      ))}
+    </>
+  );
+};
+
+const Total = ({ parts }) => {
+  return (
+    <p>
+      Number of exercises {parts.reduce((acc, part) => acc + part.exercises, 0)}
+    </p>
+  );
+};
+
+const App = () => {
+  const course = {
+    name: "Half Stack application development",
+    parts: [
+      {
+        name: "Fundamentals of React",
+        exercises: 10,
+      },
+      {
+        name: "Using props to pass data",
+        exercises: 7,
+      },
+      {
+        name: "State of a component",
+        exercises: 14,
+      },
+    ],
+  };
+
+  return (
+    <div>
+      <Header course={course} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
+    </div>
+  );
+};
+
+export default App;
