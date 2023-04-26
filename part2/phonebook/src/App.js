27e813a7240811894da418efde7525de97e2b59b
Sami Luomansuu <sami.luomansuu@gmail.com>
2023-04-26 06:06:36 +0300

    Added part2 phonebook. Exercises 2.6-2.10 done.

---
+++ part2/phonebook/src/App.js
@@ -0,0 +1,84 @@
+import { useState } from "react";
+
+const Filter = ({ filter, setFilter }) => {
+  return (
+    <div>
+      filter shown with{" "}
+      <input value={filter} onChange={(e) => setFilter(e.target.value)} />
+    </div>
+  );
+};
+
+const PersonForm = ({ persons, setPersons }) => {
+  const [newName, setNewName] = useState("");
+  const [newNumber, setNewNumber] = useState("");
+
+  const submit = (event) => {
+    event.preventDefault();
+
+    if (persons.some((person) => person.name === newName)) {
+      alert(`${newName} is already added to phonebook`);
+      return;
+    }
+
+    setPersons([...persons, { name: newName, number: newNumber }]);
+  };
+
+  return (
+    <form onSubmit={submit}>
+      <div>
+        name:{" "}
+        <input value={newName} onChange={(e) => setNewName(e.target.value)} />
+      </div>
+      <div>
+        number:{" "}
+        <input
+          value={newNumber}
+          onChange={(e) => setNewNumber(e.target.value)}
+        />
+      </div>
+      <div>
+        <button type="submit">add</button>
+      </div>
+    </form>
+  );
+};
+
+const Persons = ({ filter, persons }) => {
+  const filteredPersons = persons.filter((person) =>
+    person.name.toLowerCase().includes(filter.toLowerCase())
+  );
+
+  return (
+    <div>
+      {filteredPersons.map((person) => (
+        <div key={person.name}>
+          {person.name} {person.number}
+        </div>
+      ))}
+    </div>
+  );
+};
+
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: "Arto Hellas", number: "040-123456" },
+    { name: "Ada Lovelace", number: "39-44-5323523" },
+    { name: "Dan Abramov", number: "12-43-234345" },
+    { name: "Mary Poppendieck", number: "39-23-6423122" },
+  ]);
+  const [filter, setFilter] = useState("");
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <Filter filter={filter} setFilter={setFilter} />
+      <h2>add a new</h2>
+      <PersonForm persons={persons} setPersons={setPersons} />
+      <h2>Numbers</h2>
+      <Persons filter={filter} persons={persons} />
+    </div>
+  );
+};
+
+export default App;
