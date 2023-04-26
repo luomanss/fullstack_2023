f0430ad2167c3adc7e0671348ecd12ad3938ec85
Sami Luomansuu <sami.luomansuu@gmail.com>
2023-04-26 06:18:53 +0300

    Added db.json to part2 phonebook and modified app to fetch initial state from it. Exercise 2.11 done.

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const Filter = ({ filter, setFilter }) => {
   return (
@@ -61,13 +61,17 @@ const Persons = ({ filter, persons }) => {
 };
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: "040-123456" },
-    { name: "Ada Lovelace", number: "39-44-5323523" },
-    { name: "Dan Abramov", number: "12-43-234345" },
-    { name: "Mary Poppendieck", number: "39-23-6423122" },
-  ]);
+  const [persons, setPersons] = useState([]);
   const [filter, setFilter] = useState("");
+
+  useEffect(() => {
+    (async () => {
+      const response = await fetch("http://localhost:3001/persons");
+      const data = await response.json();
+      
+      setPersons(data);
+    })();
+  }, []);
 
   return (
     <div>
