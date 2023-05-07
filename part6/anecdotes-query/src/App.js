301290c52ddfbda54867405c6d0d7423f205bede
Sami Luomansuu <sami.luomansuu@gmail.com>
2023-05-07 08:38:26 +0300

    Part6 anecdotes-query: Added React Query
    -(feat) Adding anecdotes uses React Query
    -(feat) Updating votes uses React Query
    
    Project: Exercises 6.20-6.22 done

--- part6/anecdotes-query/src/App.js
+++ part6/anecdotes-query/src/App.js
@@ -1,40 +1,70 @@
-import AnecdoteForm from './components/AnecdoteForm'
-import Notification from './components/Notification'
+import { useQuery, useMutation, useQueryClient } from "react-query";
+import axios from "axios";
+import AnecdoteForm from "./components/AnecdoteForm";
+import Notification from "./components/Notification";
 
 const App = () => {
+  const queryClient = useQueryClient();
+
+  const voteMutation = useMutation(
+    async (anecdote) => {
+      axios.put(`/anecdotes/${anecdote.id}`, anecdote);
+
+      return anecdote;
+    },
+    {
+      onSuccess: (anecdote) => {
+        const previousAnecdotes = queryClient.getQueryData("anecdotes");
+
+        queryClient.setQueryData(
+          "anecdotes",
+          previousAnecdotes.map((previousAnecdote) =>
+            previousAnecdote.id === anecdote.id ? anecdote : previousAnecdote
+          )
+        );
+      },
+    }
+  );
 
   const handleVote = (anecdote) => {
-    console.log('vote')
+    voteMutation.mutate({
+      ...anecdote,
+      votes: anecdote.votes + 1,
+    });
+  };
+
+  const result = useQuery("anecdotes", async () => {
+    const result = await axios.get("/anecdotes");
+
+    return result.data;
+  });
+
+  if (result.isLoading) {
+    return <div>Loading...</div>;
+  } else if (result.isError) {
+    return <div>anecodote service not available due to problems in server</div>;
   }
 
-  const anecdotes = [
-    {
-      "content": "If it hurts, do it more often",
-      "id": "47145",
-      "votes": 0
-    },
-  ]
+  const anecdotes = result.data;
 
   return (
     <div>
       <h3>Anecdote app</h3>
-    
+
       <Notification />
       <AnecdoteForm />
-    
-      {anecdotes.map(anecdote =>
+
+      {anecdotes.map((anecdote) => (
         <div key={anecdote.id}>
-          <div>
-            {anecdote.content}
-          </div>
+          <div>{anecdote.content}</div>
           <div>
             has {anecdote.votes}
             <button onClick={() => handleVote(anecdote)}>vote</button>
           </div>
         </div>
-      )}
+      ))}
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- part6/anecdotes-query/src/components/AnecdoteForm.js
+++ part6/anecdotes-query/src/components/AnecdoteForm.js
@@ -1,21 +1,48 @@
+import { useMutation, useQueryClient } from "react-query";
+import axios from "axios";
+
 const AnecdoteForm = () => {
+  const queryClient = useQueryClient();
+
+  const createMutation = useMutation(
+    async (anecdote) => {
+      const result = await axios.post("/anecdotes", anecdote);
+
+      return result.data;
+    },
+    {
+      onSuccess: (newAnecdote) => {
+        const previousAnecdotes = queryClient.getQueryData("anecdotes");
+
+        queryClient.setQueryData(
+            "anecdotes",
+          previousAnecdotes.concat(newAnecdote)
+        );
+      },
+    }
+  );
 
   const onCreate = (event) => {
-    event.preventDefault()
-    const content = event.target.anecdote.value
-    event.target.anecdote.value = ''
-    console.log('new anecdote')
-}
+    event.preventDefault();
+
+    const content = event.target.anecdote.value;
+    event.target.anecdote.value = "";
+
+    createMutation.mutate({
+      content,
+      votes: 0,
+    });
+  };
 
   return (
     <div>
       <h3>create new</h3>
       <form onSubmit={onCreate}>
-        <input name='anecdote' />
+        <input name="anecdote" />
         <button type="submit">create</button>
       </form>
     </div>
-  )
-}
+  );
+};
 
-export default AnecdoteForm
+export default AnecdoteForm;
