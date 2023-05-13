01cacc9ecd6d2a99c4194aeb0d87626e50cf01be
Sami Luomansuu <sami.luomansuu@gmail.com>
2023-05-13 06:02:12 +0300

    Part7 bloglist: Added Chakra UI to the app

--- part7/bloglist/src/index.js
+++ part7/bloglist/src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
 import { RouterProvider } from "react-router-dom";
+import { ChakraProvider } from "@chakra-ui/react";
 import "./index.css";
 
 import store from "./store";
@@ -9,6 +10,8 @@ import router from "./router";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <Provider store={store}>
-    <RouterProvider router={router} />
+    <ChakraProvider>
+      <RouterProvider router={router} />
+    </ChakraProvider>
   </Provider>
 );
